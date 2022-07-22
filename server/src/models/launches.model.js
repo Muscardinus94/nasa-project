34d2efa98b76c141acb03a5a3d3611b36af09c37
Jae Kim <9028364@gmail.com>
2022-07-23 02:38:09 +0900

    POST /launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
@@ -13,11 +15,22 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(latestFlightNumber, Object.assign(launch, {
+    flightNumber: latestFlightNumber,
+    customers: ["Zero To Mastery", "NASA"],
+    upcoming: true,
+    success: true,
+  }));
+}
+
 function getAllLaunches() {
-  return Array.from(launches.value());
+  return Array.from(launches.values());
 }
 
 module.exports = {
   launches,
-  getAllLaunches
+  getAllLaunches,
+  addNewLaunch
 }
